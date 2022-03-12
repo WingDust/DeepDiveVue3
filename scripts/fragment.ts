38f6addb580c5e575a12ba985c7841207c302549
wingdust <995315823@qq.com>
2022-03-12 20:24:28 +0800

    auto-commit

--- scripts/fragment.ts
+++ scripts/fragment.ts
@@ -24,7 +24,7 @@ interface Fragments{
 const prettier = (obj:string|Object)=>{
   switch (typeof(obj)) {
     case 'string':{
-      console.log(JSON.stringify(JSON.parse(obj),null,));
+      console.log(JSON.stringify(JSON.parse(obj),null,2));
       
     }
       
